cb96fbe9adecd1e22ad36230cac83362ba9cf8ca
ERFAN <erfan.shokr300@gmail.com>
2023-11-14 17:47:03 +0330

    show list item in layout page in first commit.

--- product_module/views.py
+++ product_module/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_module/product_list.html', {'products': products})
