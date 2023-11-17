7b41aa12594795e524778227ed6db532bee872de
ERFAN <erfan.shokr300@gmail.com>
2023-11-17 15:59:51 +0330

    added detail item in the new url address. And still updating ...

--- product_module/models.py
+++ product_module/models.py
@@ -11,4 +11,3 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.price})"
-
--- product_module/views.py
+++ product_module/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from django.http import Http404
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_module/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except:
+        raise Http404()
+    # product = get_object_or_404(Product, pk=product_id)==> this code working instent of try and except
+    return render(request, 'product_module/product_detail.html', {'product': product})
