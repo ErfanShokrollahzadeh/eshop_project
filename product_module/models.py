c684e3deb09dd4536e6e50deadd7305dbc1aca4c
ERFAN <erfan.shokr300@gmail.com>
2023-11-17 16:22:13 +0330

    added revers method in main class to show details item

--- product_module/models.py
+++ product_module/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -8,6 +9,9 @@ class Product(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
     short_description = models.CharField(max_length=360, null=True)
     is_active = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[str(self.id)])
 
     def __str__(self):
         return f"{self.title} ({self.price})"
