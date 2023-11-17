d1ea181c869a1965176865c4716272024e1b16e9
ERFAN <erfan.shokr300@gmail.com>
2023-11-17 17:15:01 +0330

    added number of product in layout page.

--- product_module/views.py
+++ product_module/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Product
 from django.http import Http404
+from django.db.models import Avg, Max, Min
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'product_module/product_list.html', {'products': products})
+    number_of_products = products.count()
+    avg_rating = products.aggregate(Avg('rating')),
+    return render(request, 'product_module/product_list.html',
+                  {'products': products,
+                   'total_number_of_products': number_of_products,
+                   'avarage_rating': avg_rating
+    })
 
 def product_detail(request, slug):
     try:
