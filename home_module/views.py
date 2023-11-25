5cfef80a9f4701a1c1d978b85e2cc0cd26254661
ERFAN <erfan.shokr300@gmail.com>
2023-11-25 17:55:42 +0330

    update and fixed contact us form and use post and get method in forms

--- home_module/views.py
+++ home_module/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 def index_page(request):
     return render(request, 'home_module/index_page.html')
 
-def contact_page(request):
-    return render(request, 'home_module/contact_page.html')
-
 def site_header_component(request):
     context = {
         'link': 'اموزش چنگو'
