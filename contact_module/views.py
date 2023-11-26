b403a5c8dcd9edcd277403e02d4cb8ea4897fd10
ERFAN <erfan.shokr300@gmail.com>
2023-11-26 13:02:19 +0330

    set up post and get method in contact us page from users

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+# from django.http import HttpResponseRedirect
 
 
 def contact_us_page(request):
+    if request.method == 'POST':
+        print(request.POST['email'])
+        print(request.POST['fullname'])
+        print(request.POST['subject'])
+        print(request.POST['message'])
+        return redirect(reverse('home_page'))
     return render(request, 'contact_module/contact_us_page.html', {})
--- home_module/urls.py
+++ home_module/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='index_page'),
+    path('', views.index_page, name='home_page'),
     # path('site-header', views.site_header_partial, name='site_header_partial'),
 ]
