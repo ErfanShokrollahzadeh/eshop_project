1af22aa3032b6116c3d0c641df8401c445b0bb96
ERFAN <erfan.shokr300@gmail.com>
2023-11-27 16:48:49 +0330

    set up post and get method in contact us page from users. and validation labels for check everything is correct or not section 3

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 
 class ContactUsForm(forms.Form):
-    fullname = forms.CharField(max_length=300, label='نام و نام خانوادگی')
-    email = forms.EmailField(max_length=300, label='ایمیل', widget=forms.EmailInput)
-    subject = forms.CharField(max_length=300, label='عنوان')
-    text = forms.CharField(widget=forms.Textarea, label='متن پیام')
+    fullname = forms.CharField(max_length=50, label='نام و نام خانوادگی',
+        error_messages={'max_length': 'لطفا نام و نام خانوادگی نمی تواند بیشتر از ۵۰ کاراکتر باشد'},
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'نام نام خانوادگی'}))
+
+    email = forms.EmailField(max_length=300, label='ایمیل',
+            widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'ایمیل'}))
+
+    subject = forms.CharField(max_length=300, label='عنوان',
+            widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'عنوان'}))
+
+    text = forms.CharField(label='متن پیام',
+            widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'متن پیام', 'id':'message', 'rows':'5'}))
