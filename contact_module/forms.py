951a403df89f218ff10f29b4900b08b755c35419
ERFAN <erfan.shokr300@gmail.com>
2023-11-29 18:25:13 +0330

    creat model forms how to better than and esy post data from contact us page.

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -24,3 +24,21 @@ class ContactUsModelFrom(forms.ModelForm):
         model = ContactUs
         fields = ['title', 'full_name', 'email', 'message'] # or for all we can use fields: '__all__'
         # exclude=['response'] # it's mean I just don't want to show this field in form
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'عنوان'}),
+            'full_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'نام و نام خانوادگی'}),
+            'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'ایمیل'}),
+            'message': forms.Textarea(attrs={'class':'form-control', 'placeholder':'متن پیام', 'id':'message', 'rows':'5'}),
+        }
+        labels = {
+            'title': 'عنوان',
+            'full_name': 'نام و نام خانوادگی',
+            'email': 'ایمیل',
+            'message': 'متن پیام',
+        }
+        error_messages = {
+            'full_name': {
+                'required': 'لطفا نام و نام خانوادگی خود را وارد کنید',
+                'max_length': 'لطفا نام و نام خانوادگی نمی تواند بیشتر از ۵۰ کاراکتر باشد',
+            },
+        }
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,33 +1,42 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views import View
 # from django.http import HttpResponseRedirect
 from .forms import ContactUsForm, ContactUsModelFrom
 from .models import ContactUs
 
 
-def contact_us_page(request):
-    # if request.method == 'POST':
-    #     entered_email = request.POST['email']
-    #     if entered_email == '':
-    #         return render(request, 'contact_module/contact_us_page.html', {'has_error': True})
-    #     print(request.POST['email'])
-    #     print(request.POST['fullname'])
-    #     print(request.POST['subject'])
-    #     print(request.POST['message'])
-    #     return redirect(reverse('home_page'))
+class ContactUsView(View):
+    def get(self, request):
+        contact_form = ContactUsModelFrom()
+        return render(request, 'contact_module/contact_us_page.html', {'contact_form': contact_form})
 
+    def post(self, request):
+        contact_form = ContactUsModelFrom(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            return redirect(reverse('home_page'))
+        else:
+            return render(request, 'contact_module/contact_us_page.html', {'contact_form': contact_form})
+
+
+
+
+
+def contact_us_page(request):
     if request.method == 'POST':
         # contact_form = ContactUsForm(request.POST)
         contact_form = ContactUsModelFrom(request.POST)
         if contact_form.is_valid():
-            print(contact_form.cleaned_data)
-            contact = ContactUs(
-                title = contact_form.cleaned_data.get('title'),
-                full_name=contact_form.cleaned_data.get('full_name'),
-                email=contact_form.cleaned_data.get('email'),
-                message=contact_form.cleaned_data.get('message'),
-            )
-            contact.save()
+            # print(contact_form.cleaned_data)
+            # contact = ContactUs(
+            #     title = contact_form.cleaned_data.get('title'),
+            #     full_name=contact_form.cleaned_data.get('full_name'),
+            #     email=contact_form.cleaned_data.get('email'),
+            #     message=contact_form.cleaned_data.get('message'),
+            # )
+            # contact.save()
+            contact_form.save()
             return redirect(reverse('home_page'))
     else:
         # contact_form = ContactUsForm()
