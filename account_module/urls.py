fa0db1ccfbc8c7054d9162b9cf22a7e206cfa1ff
ERFAN <erfan.shokr300@gmail.com>
2023-12-15 00:50:07 +0330

    compleat log in user in site, yet still updating...

--- account_module/urls.py
+++ account_module/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register_page'),
+    path('login/', views.LoginView.as_view(), name='login_page'),
 ]
--- account_module/views.py
+++ account_module/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # from django.contrib.auth import get_user_model
 from django.views import View
 from account_module.forms import RegisterForm
@@ -19,14 +20,38 @@ class RegisterView(View):
             user_password = register_form.cleaned_data.get('password')
             user: bool = User.objects.filter(email__iexact=user_email).exists()
             if user:
-                register_form.add_error('email','ایمیل واد شده تکراری می باشد')
+                register_form.add_error('email', 'ایمیل واد شده تکراری می باشد')
             else:
-                new_user = User.objects.create_user(email=user_email, password=user_password)
+                new_user = User(email=user_email, email_active_code=get_random_string(length=72), is_active=False,
+                                username=user_email)
+                new_user.set_password(user_password)
                 new_user.save()
-
-
-
-
+                # todo : send email active code
+                return redirect(reverse('login_page'))
 
         context = {'register_form': register_form}
         return render(request, 'account_module/register.html', context)
+
+
+class LoginView(View):
+    def get(self, request):
+        context = {'login_form': None}
+        return render(request, 'account_module/register.html', context)
+
+    def post(self, request):
+        pass
+        # register_form = RegisterForm(request.POST)
+        # if register_form.is_valid():
+        #     user_email = register_form.cleaned_data.get('email')
+        #     user_password = register_form.cleaned_data.get('password')
+        #     user: bool = User.objects.filter(email__iexact=user_email).exists()
+        #     if user:
+        #         register_form.add_error('email', 'ایمیل واد شده تکراری می باشد')
+        #     else:
+        #         new_user = User(email=user_email, email_active_code=get_random_string(length=72))
+        #         new_user.set_password(user_password)
+        #         new_user.save()
+        #         # todo : send email active code
+        #
+        # context = {'register_form': register_form}
+        # return render(request, 'account_module/register.html', context)
