0cdf01b36e0aab1ee4a95828a0afeca4ff37ff00
ERFAN <erfan.shokr300@gmail.com>
2023-12-18 17:16:43 +0330

    Complete the user login section and use the site

--- account_module/views.py
+++ account_module/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import HttpRequest
 # from django.contrib.auth import get_user_model
 from django.views import View
 from account_module.forms import RegisterForm
@@ -7,6 +8,7 @@ from .models import User
 from django.utils.crypto import get_random_string
 from django.http import Http404
 from .forms import LoginForm
+from django.contrib.auth import login, logout
 
 
 class RegisterView(View):
@@ -41,23 +43,27 @@ class LoginView(View):
         context = {'login_form': login_form}
         return render(request, 'account_module/login.html', context)
 
-    def post(self, request):
-        pass
-        # register_form = RegisterForm(request.POST)
-        # if register_form.is_valid():
-        #     user_email = register_form.cleaned_data.get('email')
-        #     user_password = register_form.cleaned_data.get('password')
-        #     user: bool = User.objects.filter(email__iexact=user_email).exists()
-        #     if user:
-        #         register_form.add_error('email', 'ایمیل واد شده تکراری می باشد')
-        #     else:
-        #         new_user = User(email=user_email, email_active_code=get_random_string(length=72))
-        #         new_user.set_password(user_password)
-        #         new_user.save()
-        #         # todo : send email active code
-        #
-        # context = {'register_form': register_form}
-        # return render(request, 'account_module/register.html', context)
+    def post(self, request: HttpRequest):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            # user_email = login_form.cleaned_data.get('email')
+            user_password = login_form.cleaned_data.get('password')
+            user: User = User.objects.filter(email__iexact=login_form.cleaned_data.get('email')).first()
+            if user is not None:
+                if  not user.is_active:
+                    login_form.add_error('email', 'حساب کاربری شما فعال نیست')
+                else:
+                    is_password_correct = user.check_password(user_password)
+                    if is_password_correct:
+                        login(request, user)
+                        return redirect(reverse('home_page'))
+                    else:
+                        login_form.add_error('email', 'کاربری یافت نشد')
+            else:
+                login_form.add_error('email', 'کاربری با این مشخصات یافت نشد')
+
+        context = {'login_form': login_form}
+        return render(request, 'account_module/login.html', context)
 
 
 class ActivateAccountView(View):
