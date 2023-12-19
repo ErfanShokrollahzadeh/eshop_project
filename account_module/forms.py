c8c58b4d0b6c4d02d69a2b46f9739f6698f717ca
ERFAN <erfan.shokr300@gmail.com>
2023-12-19 14:07:18 +0330

    create reset password for who user to forgot pass account for login and still this section is updating...

--- account_module/forms.py
+++ account_module/forms.py
@@ -17,9 +17,26 @@ class RegisterForm(forms.Form):
         return confrim_password
 
 
-
 class LoginForm(forms.Form):
     email = forms.EmailField(label='ایمیل', widget=forms.EmailInput,
                              validators=[validators.MaxLengthValidator(100), validators.EmailValidator])
     password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput,
                                validators=[validators.MaxLengthValidator(100)])
+
+
+class ForgotPasswordForm(forms.Form):
+    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput,
+                             validators=[validators.MaxLengthValidator(100), validators.EmailValidator])
+
+
+class ResetPasswordForm(forms.Form):
+    password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput,
+                               validators=[validators.MaxLengthValidator(100)])
+    confrim_password = forms.CharField(label='تکرار رمز عبور', widget=forms.PasswordInput)
+
+    def clean_confrim_password(self):
+        password = self.cleaned_data.get('password')
+        confrim_password = self.cleaned_data.get('confrim_password')
+        if password != confrim_password:
+            raise forms.ValidationError('رمز عبور و تکرار آن یکسان نیستند')
+        return confrim_password
--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-q1aufzsexrcxrjws%%8f_vgq5lt74zu@5@cnsm0u*vx$eqyc=0
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -46,6 +44,7 @@ INSTALLED_APPS = [
     'django_render_partial',
 
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -78,12 +77,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eshop_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-AUTH_USER_MODEL  = 'account_module.User' # for working with custom user model
+AUTH_USER_MODEL = 'account_module.User'  # for working with custom user model
 
 DATABASES = {
     'default': {
@@ -91,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -111,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -122,7 +118,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -136,11 +131,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # SESSION_COOKIE_AGE = 120 # 2 minutes
