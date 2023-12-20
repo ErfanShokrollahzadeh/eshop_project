02427ef56e773d95b4f5c85fa2802c8e4c4023d6
ERFAN <erfan.shokr300@gmail.com>
2023-12-20 20:22:47 +0330

    Compleat dynamic slider section and change some item and subject of shop online

--- site_module/admin.py
+++ site_module/admin.py
@@ -4,7 +4,11 @@ from . import models
 class FooterLinkAdmin(admin.ModelAdmin):
     list_display = ['title', 'url']
 
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'is_active']
+    list_editable = ['url', 'is_active']
 
 admin.site.register(models.SiteSetting)
 admin.site.register(models.FooterLinkBox)
+admin.site.register(models.Slider,SliderAdmin)
 admin.site.register(models.FooterLink, FooterLinkAdmin)
