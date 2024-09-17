7eea0ddfcf97ba2902cc0b851ce0fcf1b474c847
ERFAN <erfan.shokr300@gmail.com>
2024-09-17 16:15:34 +0400

    Improve performance and fix mirror bugs.

--- contact_module/models.py
+++ contact_module/models.py
@@ -9,9 +9,12 @@ class ContactUs(models.Model):
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='created date') # auto_now_add=True means that this field will be filled automatically with the current date when the object is created
     response = models.TextField(blank=True, null=True, verbose_name='response')
     is_read_by_admin = models.BooleanField(default=False, verbose_name='is read by admin')
+    response = models.TextField(blank=True, null=True, verbose_name='response')
 
 
-    class Meta:  # change name of table in admin panel from 'product brands' to 'brands
+
+
+    class Meta:  # change name of table in admin panel from 'product brands' to brands
         verbose_name = 'contact us'
         verbose_name_plural = 'contact us list'
 
